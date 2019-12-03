a6f4276f22acb58bf44d4907102a1418d58f371c
kallelrm <kallelr@outlook.com>
2019-12-03 17:05:44 -0300

    Criando validações nos agendamentos

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -5,7 +5,7 @@ class Appointment extends Model {
     super.init(
       {
         date: Sequelize.DATE,
-        cancelled_at: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
       },
       {
         sequelize,
