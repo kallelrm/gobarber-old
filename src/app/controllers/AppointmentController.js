e240c780cc19108278302cd33d0854f24fe930a9
kallelrm <kallelr@outlook.com>
2019-12-15 15:23:10 -0300

    Instalando e configurando nodemailer | Usando nodemailer para notificar o provider do cancelamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -8,6 +8,8 @@ import File from '../models/File';
 import Notification from '../schemas/Notification';
 import Appointment from '../models/Appointment';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
@@ -122,7 +124,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -139,7 +149,14 @@ class AppointmentController {
     }
 
     appointment.canceled_at = new Date();
+
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento',
+    });
 
     return res.json({ appointment });
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '5a6d9f0b764c8c',
+    pass: '45f53de66544fa',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gorbarber.com>',
+  },
+};
